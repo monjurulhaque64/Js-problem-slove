17a2269f3c873c3fea5fa9327e3da71c306c4b21
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-07-27 01:14:49 +0600

    task 7 done

---
+++ task7.js
@@ -0,0 +1,38 @@
+// Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
+
+
+
+function romanToInteger(roman) {
+    const romanNumerals = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+  
+    let result = 0;
+    let prevValue = 0;
+  
+    for (let i = roman.length - 1; i >= 0; i--) {
+      const currentNumeral = roman[i];
+      const currentValue = romanNumerals[currentNumeral];
+  
+      if (currentValue < prevValue) {
+        result -= currentValue;
+      } else {
+        result += currentValue;
+      }
+  
+      prevValue = currentValue;
+    }
+  
+    return result;
+  }
+  
+  // Example usage:
+  console.log(romanToInteger("IX")); 
+  console.log(romanToInteger("XXI")); 
+  
